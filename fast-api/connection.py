bc10ed3d1911c74f6cba3fde529392b52ccb85b1
Diana Adrover <diana.adrover@merck.com>
2024-09-19 23:59:19 -0300

    env

--- fast-api/connection.py
+++ fast-api/connection.py
@@ -1,9 +1,16 @@
+import os
 import cx_Oracle
 
 def con():
-    dsn = cx_Oracle.makedsn(host='EXADB03PR-SCAN-ALSC.MERCK.COM', port=1521, service_name='PMCKCI')
-    connection = cx_Oracle.connect(user='adroverd_4938', password='Nanita!20245', dsn=dsn)
-    print("dns ===>>>> " + dsn)
+    host = os.getenv('DB_HOST')
+    port = os.getenv('DB_PORT')
+    service_name = os.getenv('DB_SERVICE_NAME')
+    user = os.getenv('DB_USER')
+    password = os.getenv('DB_PASSWORD')
+
+    dsn = cx_Oracle.makedsn(host=host, port=port, service_name=service_name)
+    connection = cx_Oracle.connect(user=user, password=password, dsn=dsn)
+    print("dsn ===>>>> " + dsn)
     return connection
 
 # SIMIL ORM
